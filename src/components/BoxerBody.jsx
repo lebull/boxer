194cbed856e59ef57f046c52940060c1b9697ef4
lebull <bulldogboy912000@yahoo.com>
2020-05-24 09:02:43 -0500

    Cardify

--- src/components/BoxerBody.jsx
+++ src/components/BoxerBody.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, CardContent } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 
 export default function BoxerBody() {
     return (
-        <div>
-            <Typography variant="h1" component="h1">
-                Boxer
+        <Card>
+            <CardContent>
+                <Typography variant="h1" component="h1">
+                    Boxer
             </Typography>
-            <Typography variant="h2" component="h2">
-                Goal
+                <Typography variant="h2" component="h2">
+                    Goal
             </Typography>
-            <Typography variant="p" component="p">
-                Using the fitbit api (or whatever else can be throw in), compare weight progress over a set time.  Only reveal progress on a certain "weigh-in" day.
+                <Typography variant="p" component="p">
+                    Using the fitbit api (or whatever else can be throw in), compare weight progress over a set time.  Only reveal progress on a certain "weigh-in" day.
             </Typography>
-            <Typography variant="h2" component="h2">
-                Datatypes
+                <Typography variant="h2" component="h2">
+                    Datatypes
             </Typography>
-            <Typography variant="p" component="p">
-                <ul>
-                    <li>Body wibly wobbly and also auth</li>
-                    <li>Competition</li>
-                    <li>I guess that's about it?</li>
-                </ul>
-            </Typography>
-        </div>
+                <Typography variant="p" component="p">
+                    <ul>
+                        <li>Profile Crap, look at firebase plz</li>
+                        <li>Body wibly wobbly and also auth</li>
+                        <li>Competition</li>
+                        <li>I guess that's about it?</li>
+                    </ul>
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
